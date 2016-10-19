75ce45bde98921cacf8b1f5ab57e62589e5e7125
shiyunlai <shiyunlai@gmail.com>
2016-10-19 15:43:50 +0800

    add in-action-springmvc
    新增spring mvc 的例子工程

---
+++ in-action-springmvc/sql/table.sql
@@ -0,0 +1,14 @@
+use springmvc;
+
+create table t_dept (
+    id int primary key auto_increment,
+    name varchar(200)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+create table t_employee (
+    id int primary key auto_increment,
+    age int ,
+    name varchar(200),
+    dept_id int references t_dept(id)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
